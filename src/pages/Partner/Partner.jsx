bf5b714bb43ec2b255951097461e9da8d6a5bf9f
sajibkhan33 <nurnavi929@gmail.com>
2025-12-18 19:00:07 +0600

    blog page image done

--- src/pages/Partner/Partner.jsx
+++ src/pages/Partner/Partner.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import partner from "../../../src/assets/partner/partner.jpg"
 
 const Partner = () => {
   return (
     <div className="container mx-auto p-8">
-      Partner......
-      <span className="loading loading-spinner text-primary"></span>
-      <span className="loading loading-spinner text-secondary"></span>
-      <span className="loading loading-spinner text-accent"></span>
-      <span className="loading loading-spinner text-neutral"></span>
-      <span className="loading loading-spinner text-info"></span>
-      <span className="loading loading-spinner text-success"></span>
-      <span className="loading loading-spinner text-warning"></span>
-      <span className="loading loading-spinner text-error"></span>
+      <div className="card bg-base-100 image-full w-100% shadow-sm">
+        <figure>
+          <img
+            src={partner}
+            alt="Shoes"
+            className="w-full object-cover"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+          
+        </div>
+      </div>
     </div>
   );
 };
