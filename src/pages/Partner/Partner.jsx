63520ef10ce13210ae3758dc918bc03c52538737
sajibkhan33 <nurnavi929@gmail.com>
2025-12-19 15:43:41 +0600

    partner page done

--- src/pages/Partner/Partner.jsx
+++ src/pages/Partner/Partner.jsx
@@ -1,26 +1,15 @@
 import React from "react";
-import partner from "../../../src/assets/partner/partner.jpg"
+import PartnerHero from "./PartnerHero/PartnerHero";
+import PartnerBenefits from "./PartnerBenefits/PartnerBenefits";
+import PartnerPolicy from "./PartnerPolicy/PartnerPolicy";
+
 
 const Partner = () => {
   return (
-    <div className="container mx-auto p-8">
-      <div className="card bg-base-100 image-full w-100% shadow-sm">
-        <figure>
-          <img
-            src={partner}
-            alt="Shoes"
-            className="w-full object-cover"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          
-        </div>
-      </div>
+    <div>
+      <PartnerHero/>
+      <PartnerBenefits/>
+      <PartnerPolicy/>
     </div>
   );
 };
